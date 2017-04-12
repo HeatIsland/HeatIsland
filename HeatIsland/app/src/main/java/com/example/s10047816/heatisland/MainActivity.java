fc902775156217673970b3fc8a07581b0113aabd
Alexander Wlezien <alexander.wlezienh4@stu.austinisd.org>
2017-04-12 15:29:03 -0500

    Adding new Android Studio Project

---
+++ HeatIsland/app/src/main/java/com/example/s10047816/heatisland/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.s10047816.heatisland;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
